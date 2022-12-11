1fd72528f66dafa264947a070eff260a4c0d7b49
lerkush <lutfiyye@gmail.com>
2022-12-11 23:05:27 +0300

    It is set up database and models

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
+MEDIA_URL = 'images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
--- store/views.py
+++ store/views.py
@@ -1,17 +1,36 @@
 from django.shortcuts import render
-
+from .models import *
 # Create your views here.
 
 def store(request):
-    context = {}
+    products = Product.objects.all()
+    context = {'products':products}
     return render(request, 'store/store.html', context)
 
 def cart(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+
+    context = {'items':items, 'order': order}
     return render(request, 'store/cart.html', context)
 
 def checkout(request):
-    context = {}
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.orderitem_set.all()
+    else:
+        #Create Empty cart for now for none-logged in users
+        items = []
+        order = {'get_cart_total':0, 'get_cart_items':0}
+        
+
+    context = {'items':items, 'order': order}
     return render(request, 'store/checkout.html', context)
 
 
